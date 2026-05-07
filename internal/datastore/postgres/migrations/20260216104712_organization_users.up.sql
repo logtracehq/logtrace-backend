c7c762f169dfc786aede76755ce5b8118e2b19b1
Luigi Morel <morelmiles@gmail.com>
2026-05-07 13:03:56 +0300

    fix: rename column 'name' to 'username' in organization_users table

--- internal/datastore/postgres/migrations/20260216104712_organization_users.up.sql
+++ internal/datastore/postgres/migrations/20260216104712_organization_users.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE organization_users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
     organization_id UUID NOT NULL,
     user_id VARCHAR,
-    name VARCHAR,
+    username VARCHAR,
     metadata JSONB,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
